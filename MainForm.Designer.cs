080c324f253ef17324facf4c176e85722dfc573a
tinaut1986 <tinaut1986@gmail.com>
2024-04-20 20:42:34 +0200

    Initial commit

---
+++ MainForm.Designer.cs
@@ -0,0 +1,170 @@
+﻿
+namespace Test_web
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            webView = new Microsoft.Web.WebView2.WinForms.WebView2();
+            progressBar = new System.Windows.Forms.ProgressBar();
+            btnPlay = new System.Windows.Forms.Button();
+            txtLowerUpdate = new System.Windows.Forms.TextBox();
+            label1 = new System.Windows.Forms.Label();
+            txtUpperUpdate = new System.Windows.Forms.TextBox();
+            lblProgress = new System.Windows.Forms.Label();
+            txtURL = new System.Windows.Forms.TextBox();
+            btnOpen = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)webView).BeginInit();
+            SuspendLayout();
+            // 
+            // webView
+            // 
+            webView.AllowExternalDrop = true;
+            webView.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            webView.CreationProperties = null;
+            webView.DefaultBackgroundColor = System.Drawing.Color.White;
+            webView.Location = new System.Drawing.Point(12, 41);
+            webView.Name = "webView";
+            webView.Size = new System.Drawing.Size(1007, 447);
+            webView.TabIndex = 0;
+            webView.ZoomFactor = 1D;
+            // 
+            // progressBar
+            // 
+            progressBar.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            progressBar.Location = new System.Drawing.Point(9, 519);
+            progressBar.Name = "progressBar";
+            progressBar.Size = new System.Drawing.Size(1007, 23);
+            progressBar.TabIndex = 1;
+            // 
+            // btnPlay
+            // 
+            btnPlay.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right;
+            btnPlay.Location = new System.Drawing.Point(944, 494);
+            btnPlay.Name = "btnPlay";
+            btnPlay.Size = new System.Drawing.Size(75, 23);
+            btnPlay.TabIndex = 2;
+            btnPlay.Text = "Play";
+            btnPlay.UseVisualStyleBackColor = true;
+            btnPlay.Click += btnPlay_Click;
+            // 
+            // txtLowerUpdate
+            // 
+            txtLowerUpdate.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            txtLowerUpdate.Location = new System.Drawing.Point(126, 494);
+            txtLowerUpdate.Name = "txtLowerUpdate";
+            txtLowerUpdate.Size = new System.Drawing.Size(45, 23);
+            txtLowerUpdate.TabIndex = 3;
+            txtLowerUpdate.Text = "15";
+            // 
+            // label1
+            // 
+            label1.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(12, 498);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(108, 15);
+            label1.TabIndex = 4;
+            label1.Text = "Refresh range (seg)";
+            // 
+            // txtUpperUpdate
+            // 
+            txtUpperUpdate.Anchor = System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left;
+            txtUpperUpdate.Location = new System.Drawing.Point(177, 494);
+            txtUpperUpdate.Name = "txtUpperUpdate";
+            txtUpperUpdate.Size = new System.Drawing.Size(45, 23);
+            txtUpperUpdate.TabIndex = 3;
+            txtUpperUpdate.Text = "90";
+            // 
+            // lblProgress
+            // 
+            lblProgress.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            lblProgress.AutoSize = true;
+            lblProgress.BackColor = System.Drawing.Color.Transparent;
+            lblProgress.ForeColor = System.Drawing.Color.Black;
+            lblProgress.Location = new System.Drawing.Point(500, 523);
+            lblProgress.Name = "lblProgress";
+            lblProgress.Size = new System.Drawing.Size(24, 15);
+            lblProgress.TabIndex = 5;
+            lblProgress.Text = "0/0";
+            lblProgress.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // txtURL
+            // 
+            txtURL.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right;
+            txtURL.Location = new System.Drawing.Point(12, 12);
+            txtURL.Name = "txtURL";
+            txtURL.Size = new System.Drawing.Size(923, 23);
+            txtURL.TabIndex = 6;
+            txtURL.KeyPress += txtURL_KeyPress;
+            // 
+            // btnOpen
+            // 
+            btnOpen.Anchor = System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right;
+            btnOpen.Location = new System.Drawing.Point(941, 11);
+            btnOpen.Name = "btnOpen";
+            btnOpen.Size = new System.Drawing.Size(75, 23);
+            btnOpen.TabIndex = 2;
+            btnOpen.Text = "Open";
+            btnOpen.UseVisualStyleBackColor = true;
+            btnOpen.Click += btnOpen_Click;
+            // 
+            // MainForm
+            // 
+            AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            ClientSize = new System.Drawing.Size(1031, 558);
+            Controls.Add(txtURL);
+            Controls.Add(lblProgress);
+            Controls.Add(label1);
+            Controls.Add(txtUpperUpdate);
+            Controls.Add(txtLowerUpdate);
+            Controls.Add(btnOpen);
+            Controls.Add(btnPlay);
+            Controls.Add(progressBar);
+            Controls.Add(webView);
+            Name = "MainForm";
+            Text = "Edge search";
+            ((System.ComponentModel.ISupportInitialize)webView).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView;
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Button btnPlay;
+        private System.Windows.Forms.TextBox txtLowerUpdate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUpperUpdate;
+        private System.Windows.Forms.Label lblProgress;
+        private System.Windows.Forms.TextBox txtURL;
+        private System.Windows.Forms.Button btnOpen;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.Web.WebView2.Core;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace Test_web
+{
+    public partial class MainForm : Form
+    {
+        private Timer refreshTimer;
+        private int elapsedSeconds;
+        private int refreshSeconds;
+        private int lowerLimit;
+        private int upperLimit;
+        private Random random;
+        private bool isPlaying;
+
+        List<string> searches = new List<string>();
+
+        public MainForm()
+        {
+            InitializeComponent();
+            InitializeWebView();
+            InitializeProgressBar();
+            SetupRefreshTimer();
+            isPlaying = false;
+            random = new Random();
+
+            LoadSearchesFromFile("searches.txt"); // Carga las búsquedas desde el archivo
+
+            Load += Form_Load;
+            webView.CoreWebView2InitializationCompleted += WebView_CoreWebView2InitializationCompleted;
+        }
+
+        private void LoadSearchesFromFile(string filePath)
+        {
+            string solutionDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "../../..");
+            string finalPath = Path.Combine(solutionDir, filePath);
+
+            if (File.Exists(finalPath))
+            {
+                string[] lines = File.ReadAllLines(finalPath);
+                searches.AddRange(lines.Select(line => line.Trim())); // Añade cada línea a la lista
+            }
+            else
+            {
+                MessageBox.Show($"El archivo {finalPath} no existe.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async void WebView_CoreWebView2InitializationCompleted(object sender, CoreWebView2InitializationCompletedEventArgs e)
+        {
+            // Espera a que WebView2 esté inicializado antes de cargar la página
+            await webView.CoreWebView2.AddScriptToExecuteOnDocumentCreatedAsync(
+                       "var userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.0.0 Safari/537.36';" +
+                       "window.navigator.__defineGetter__('userAgent', function(){ return userAgent; });");
+
+            webView.Source = new Uri("https://www.bing.com/");
+            txtURL.Text = webView.Source.AbsoluteUri;
+        }
+
+        private void Form_Load(object sender, EventArgs e)
+        {
+            webView.EnsureCoreWebView2Async(null);
+        }
+
+        private async void InitializeWebView()
+        {
+            Controls.Add(webView);
+            await webView.EnsureCoreWebView2Async(); // Inicializar WebView2
+        }
+
+        private void SetupRefreshTimer()
+        {
+            refreshTimer = new Timer();
+            refreshTimer.Interval = 1000; // Intervalo de actualización cada segundo
+            refreshTimer.Tick += RefreshTimer_Tick;
+        }
+
+        private void RefreshTimer_Tick(object sender, EventArgs e)
+        {
+            elapsedSeconds++;
+
+            if (elapsedSeconds >= refreshSeconds)
+            {
+                if (searches.Count == 0)
+                {
+                    btnPlay_Click(null, null);
+                    return;
+                }
+
+                // Selecciona un elemento al azar de la lista
+                int randomIndex = random.Next(0, searches.Count);
+                string search = searches[randomIndex];
+
+                webView.Source = new Uri($"https://www.bing.com/search?q={search}");
+
+                // Elimina el elemento seleccionado para que no se repita
+                searches.RemoveAt(randomIndex);
+
+                elapsedSeconds = 0; // Reinicia el contador de segundos
+                UpdateLimits();
+                txtURL.Text = webView.Source.AbsoluteUri;
+            }
+
+            UpdateProgressBar();
+        }
+
+        private void InitializeProgressBar()
+        {
+            Controls.Add(progressBar);
+        }
+
+        private void UpdateProgressBar()
+        {
+            progressBar.Value = progressBar.Maximum - elapsedSeconds; // Calcula el progreso actual basado en los segundos transcurridos
+
+            lblProgress.Text = $"{elapsedSeconds}/{refreshSeconds} s"; // Actualiza el texto del Label
+        }
+
+        private void btnPlay_Click(object sender, EventArgs e)
+        {
+            if (!isPlaying)
+            {
+                UpdateLimits();
+                refreshTimer.Start(); // Inicia el temporizador cuando se presiona el botón
+                isPlaying = true;
+                btnPlay.Text = "Stop"; // Cambia el texto del botón a "Stop"
+            }
+            else
+            {
+                refreshTimer.Stop(); // Detiene el temporizador si ya está activo
+                isPlaying = false;
+                btnPlay.Text = "Play"; // Cambia el texto del botón a "Play"
+            }
+
+            btnOpen.Enabled = !isPlaying;
+            txtURL.Enabled = !isPlaying;
+        }
+
+        private void UpdateLimits()
+        {
+            lowerLimit = int.Parse(txtLowerUpdate.Text);
+            upperLimit = int.Parse(txtUpperUpdate.Text);
+            refreshSeconds = random.Next(lowerLimit, upperLimit + 1);
+            elapsedSeconds = 0;
+
+            UpdateProgressBarLimits();
+        }
+
+        private void UpdateProgressBarLimits()
+        {
+            progressBar.Minimum = 0;
+            progressBar.Maximum = refreshSeconds; // Número de segundos para la recarga
+            progressBar.Value = progressBar.Maximum; // Establece el valor inicial como el máximo para empezar lleno
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            webView.Source = new Uri(txtURL.Text);
+        }
+
+        private void txtURL_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == (char)ConsoleKey.Enter)
+                btnOpen_Click(null, null);
+        }
+    }
+}
