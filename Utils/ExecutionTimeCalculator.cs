a4f34cf393f6ecec66ddf0237db2ee5657cff26a
tinaut1986 <tinaut1986@gmail.com>
2024-09-07 15:43:10 +0200

    Fixed some wrong calculation when the delay between strikes is running.

--- Utils/ExecutionTimeCalculator.cs
+++ Utils/ExecutionTimeCalculator.cs
@@ -5,10 +5,15 @@ namespace EdgeSearch.Utils
     public class ExecutionTimeCalculator
     {
         public static TimeSpan CalculateTotalTime(int totalExecutions, int executionsOffset, int executionsPerStrike,
-            int pauseBetweenExecutions, int pauseBetweenStrikes, int elapsedSeconds, int currentPause, bool playing)
+            int pauseBetweenExecutions, int pauseBetweenStrikes, int elapsedSeconds, int currentPause, DateTime? strikeTime, bool playing)
         {
             if (executionsPerStrike == 0)
                 return TimeSpan.Zero;
+
+            DateTime now = DateTime.Now;
+            int strikeSeconds = 0;
+            if (strikeTime != null && playing)
+                strikeSeconds = pauseBetweenStrikes - (int)(now - strikeTime.Value).TotalSeconds;
 
             // Calculate the number of chains required to complete all executions
             int totalStrikes = (totalExecutions - 1) / executionsPerStrike;
@@ -19,7 +24,7 @@ namespace EdgeSearch.Utils
                 remainingExecutions = executionsPerStrike;
 
             // Calculate the total time for all chains except the last pause
-            int totalTimeInSeconds = (remainingExecutions * pauseBetweenExecutions) + (totalStrikes * pauseBetweenStrikes);
+            int totalTimeInSeconds = (remainingExecutions * pauseBetweenExecutions) + (totalStrikes * pauseBetweenStrikes) + strikeSeconds;
 
             // Return the total time as a TimeSpan object
             int total = totalTimeInSeconds - elapsedSeconds;
